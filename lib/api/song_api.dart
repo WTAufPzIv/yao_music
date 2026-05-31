318f56c28d91ead36e21a86c75604739a1e36b43
zxhzz <2459917510@qq.com>
2026-05-31 08:31:20 +0800

    wip

--- lib/api/song_api.dart
+++ lib/api/song_api.dart
@@ -7,7 +7,7 @@ import 'home_api.dart';
 class SongApi {
   /// 获取歌曲播放链接
   static Future<String> fetchSongDetail(SongDTO params) async {
-    final results = await dio.get('$gdMusicUrl?types=url&source=${params.platform.name}&id=${params.id}');
+    final results = await dio.get('$gdMusicUrl?types=url&source=${params.platform.name}&id=${params.id}&br=320');
     print(results.data.runtimeType);
     return params.platform == SearchPlatform.bilibili ? 'https://music-proxy.gdstudio.org/${results.data['url']}' : results.data['url'];
   }
