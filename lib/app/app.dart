d5e51b4ab726bfaf60a1a8d86af67b83db42bd05
zxhzz <2459917510@qq.com>
2026-06-02 22:09:00 +0800

    wip

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      showPerformanceOverlay: true,
+      // showPerformanceOverlay: true,
       title: 'YMusic',
       theme: ThemeData.dark(),
       home: MainPage(),
